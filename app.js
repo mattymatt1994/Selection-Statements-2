a63473da97f0d4e64a76b368c393d74ba830e58e
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-08-07 17:59:34 -0400

    update

--- app.js
+++ app.js
@@ -3,40 +3,46 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-let favNumber = 7;
-window.prompt("input a number")
-let answer = window.prompt(favNumber)
+const favNumber = 7;
+let answer = window.prompt("input a number");
 
-console.log(answer)
+console.log(answer);
 
 if (answer < favNumber) {
-  console.log("Too low!!")
-} else (answer > favNumber); {
-  console.log("too high")
-
+  console.log("Too low!!");
+} else if (answer > favNumber) {
+  console.log("too high");
 }
-if (answer = favNumber) {
-  console.log("Congratulations!!")
-} 
-
- 
-
- 
-  
-
-
-
+if (answer == favNumber) {
+  console.log("Congratulations!!");
+}
 
 // exercise 2 //
 
-birthMonth="October";
 let birthMonth = window.prompt("What is your birth month?");
 
 switch (birthMonth) {
+  case "December":
+  case "January":
+  case "February":
+    console.log("Winter..is coming");
+    break;
+  case "September":
   case "October":
-  console.log("Fall");
-
-};
+  case "November":
+    console.log("Fall");
+    break;
+  case "March":
+  case "April":
+  case "May":
+    console.log("Spring");
+    break;
+  case "June":
+  case "July":
+  case "August":
+    console.log("Summer");
+    break;
+}
 console.log(birthMonth);
 // exercise 3 //
 
@@ -49,34 +55,34 @@ let color = "BL";
 let size = "XL";
 
 switch (typeId == "01") {
-  case  "Tank top":
-break;
+  case "Tank top":
+    break;
 }
- switch (typeId =="02") {
+switch (typeId == "02") {
   case "T-Shirt":
- break;}
+    break;
+}
 
- switch (typeId == "03") {
+switch (typeId == "03") {
   case "Long Sleeve":
-  break;
+    break;
 }
- switch (typeId == "04") {
- case "Sweat Shirt":
-  break;
+switch (typeId == "04") {
+  case "Sweat Shirt":
+    break;
 }
-if (typeId == "Other") 
-
-if (colorId == "BL") {
-  color = "Black";
-} else if (colorId == "BL") {
-  color = "Blue";
-} else if (colorId == "RD") {
-  color = "Red";
-} else if (colorId == "PU") {
-  color = "Purple";
-} else {
-  color = "White";
-}
+if (typeId == "Other")
+  if (colorId == "BL") {
+    color = "Black";
+  } else if (colorId == "BL") {
+    color = "Blue";
+  } else if (colorId == "RD") {
+    color = "Red";
+  } else if (colorId == "PU") {
+    color = "Purple";
+  } else {
+    color = "White";
+  }
 
 if (sizeId == "S") {
   size = "Small";
